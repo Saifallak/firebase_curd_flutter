746bcd357b90fbf4b0a5fc1c0a44a534f3bb28a1
Saif Allah Khaled <Saifallak@yahoo.com>
2019-07-20 20:24:04 +0200

    validators class

---
+++ lib/utils/validators.dart
@@ -0,0 +1,21 @@
+class Validators {
+  static emailValidator(String email) {
+    return null;
+  }
+
+  static phoneValidator(String phone) {
+    return null;
+  }
+
+  static nameValidator(String name) {
+    return null;
+  }
+
+  static logoValidator(String logo) {
+    return null;
+  }
+
+  static locationValidator(String location) {
+    return null;
+  }
+}
