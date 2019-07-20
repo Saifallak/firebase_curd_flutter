82dbb5f7e330711ca7a0833f809e00e6d5aa642f
Saif Allah Khaled <Saifallak@yahoo.com>
2019-07-20 20:51:09 +0200

    show available users optimized

--- lib/ui/users/show_ava.dart
+++ lib/ui/users/show_ava.dart
@@ -12,9 +12,9 @@ class ShowUsersAvailable extends StatefulWidget {
 }
 
 class _ShowUsersAvailableState extends State<ShowUsersAvailable> {
+  List<UserModel> _users = List<UserModel>();
   bool _isLoading = true;
   String _error;
-  List<UserModel> _users = List<UserModel>();
 
   @override
   void initState() {
@@ -24,14 +24,78 @@ class _ShowUsersAvailableState extends State<ShowUsersAvailable> {
 
   Future<void> _initData() async {
     try {
+      _error = null;
       _users = await FirebaseRestfulApi().getAllUsers();
     } catch (e) {
-      _error = e.toString();
+      _error = "API ERROR ${e.toString()}";
     } finally {
       // make sure screen is still Alive.
       _isLoading = false;
       if (mounted) setState(() {});
     }
+  }
+
+  Widget _buildScreenBody() {
+    if (_isLoading) {
+      // Screen Still loading..
+      return LoadingModel(Colors.red);
+    } else if (_error != null) {
+      // There is an Error
+      return Center(child: Text(_error));
+    } else {
+      // Everything is OK.
+      return RefreshIndicator(
+        onRefresh: () => _initData(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: _users.length,
+                itemBuilder: (context, index) {
+                  return UserCard(
+                    userModel: _users[index],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  Future<AlertDialog> _showInfo() {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(
+            "Info",
+            style: TextStyle(
+              fontSize: 14 / MediaQuery.of(context).textScaleFactor,
+            ),
+          ),
+          content: Text(
+            "1- HOLD on User to delete\n"
+            "2- Press on '+' to add new User\n"
+            "3- Pull screen to refersh\n"
+            "4- Click on user to modify",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("okay"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -41,20 +105,14 @@ class _ShowUsersAvailableState extends State<ShowUsersAvailable> {
         backgroundColor: Colors.red,
         centerTitle: true,
         title: Text("Available Users"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () => _showInfo(),
+          ),
+        ],
       ),
-      body: _isLoading
-          ? LoadingModel(Colors.red)
-          : RefreshIndicator(
-              child: ListView.builder(
-                itemCount: _users.length,
-                itemBuilder: (context, index) {
-                  return UserCard(
-                    userModel: _users[index],
-                  );
-                },
-              ),
-              onRefresh: () => _initData(),
-            ),
+      body: _buildScreenBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.of(context).push(
           MaterialPageRoute(
