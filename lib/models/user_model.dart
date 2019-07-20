6bf921a6ed016dccb8bb77753da1aad53728595e
Saif Allah Khaled <Saifallak@yahoo.com>
2019-07-20 19:38:49 +0200

    UserModel

---
+++ lib/models/user_model.dart
@@ -0,0 +1,22 @@
+class UserModel {
+  String userId;
+  String name;
+  String email;
+  String phone;
+
+  /// Default Constructor
+  /// Use with caution as all it's variables equals to null
+  UserModel();
+
+  /// Firebase Database Constructor
+  /// Commonly used when retrieving data from Firebase Database,
+  /// or Any Provider that returns JSON/Map.
+  UserModel.fromFDatabase(Map json) {
+    assert(json != null); // Stop executing if returned data corrupted.
+
+    userId = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+  }
+}
