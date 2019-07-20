6b368ad7f36f771ce6f360d4c06991417dd5f8c9
Saif Allah Khaled <Saifallak@yahoo.com>
2019-07-20 20:24:39 +0200

    BREAKING CHANGE: user model takes a new parameter userId!

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -11,11 +11,10 @@ class UserModel {
   /// Firebase Database Constructor
   /// Commonly used when retrieving data from Firebase Database,
   /// or Any Provider that returns JSON/Map.
-  UserModel.fromFDatabase(Map json) {
+  UserModel.fromFDatabase(this.userId, Map json) {
     assert(json != null); // Stop executing if returned data corrupted.
 
     // splitting our json into the corresponding variables.
-    userId = json['id'];
     name = json['name'];
     email = json['email'];
     phone = json['phone'];
